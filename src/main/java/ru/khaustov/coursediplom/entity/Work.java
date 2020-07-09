715094989dfc8d39005b2e07cc43c0245216b245
Maksim Khaustov <haustovmaksim13@gmail.com>
2020-07-09 20:27:15 +0300

    new10

--- src/main/java/ru/khaustov/coursediplom/entity/Work.java
+++ src/main/java/ru/khaustov/coursediplom/entity/Work.java
@@ -1,11 +1,24 @@
 package ru.khaustov.coursediplom.entity;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "work_service")
 public class Work {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "service_id")
     private long id;
+
+    @Column(name = "typeOfService", unique = true, nullable = false)
     private String typeOfService;
+
+    @Column(name = "description", nullable = false, length = 1000)
     private String description;
+
+    @Column(name = "duration")
     private String duration;
-    private String prise;
+
+    @Column(name = "priсe_type")
+    private String price;
 }
