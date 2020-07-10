9e62e6ffc7ef93c1c9fd90d8532f16e2a1308d63
Maksim Khaustov <haustovmaksim13@gmail.com>
2020-07-10 20:46:31 +0300

    new13

--- src/main/java/ru/khaustov/coursediplom/contriollers/WorkController.java
+++ src/main/java/ru/khaustov/coursediplom/contriollers/WorkController.java
@@ -7,10 +7,46 @@ import org.springframework.web.bind.annotation.*;
 import ru.khaustov.coursediplom.entity.Work;
 import ru.khaustov.coursediplom.repository.WorkRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class WorkController {
     @Autowired
     private WorkRepository workRepository;
+
+    @GetMapping("admin/showAllService")
+    public String showAllService(Model model){
+        Iterable<Work> works = workRepository.findAll();
+        model.addAttribute("works", works);
+        return "admin/showAllService";
+
+    }
+
+    @GetMapping("admin/{id}")
+    public String serviceDetails(@PathVariable(value = "id") long id, Model model){
+        if (!workRepository.existsById(id)){
+            return "redirect:/admin/showAllService";
+        }
+        Optional<Work> work = workRepository.findById(id);
+        List<Work> res = new ArrayList<>();
+        work.ifPresent(res::add);
+        model.addAttribute("work", res);
+        return "admin/serviceDetails";
+    }
+
+    @GetMapping("admin/{id}/edit")
+    public String serviceEdit(@PathVariable(value = "id") long id, Model model){
+        if (!workRepository.existsById(id)){
+            return "redirect:/admin/showAllService";
+        }
+        Optional<Work> work = workRepository.findById(id);
+        List<Work> res = new ArrayList<>();
+        work.ifPresent(res::add);
+        model.addAttribute("work", res);
+        return "admin/serviceEdit";
+    }
 
 
     @GetMapping("admin/crudworks")
@@ -25,10 +61,24 @@ public class WorkController {
         return "admin/addService";
     }
 
+
     @PostMapping("admin/addService")
     public String submitForm(@ModelAttribute Work work) {
         workRepository.save(work);
         return "redirect:/admin/addService";
+    }
+
+    @PostMapping("admin/{id}/edit")
+    public String edditForm(@PathVariable(value = "id") long id, @RequestParam String typeOfService,
+                            @RequestParam String description, @RequestParam String duration,
+                            @RequestParam int price, Model model) {
+        Work work = workRepository.findById(id).orElseThrow();
+        work.setTypeOfService(typeOfService);
+        work.setDescription(description);
+        work.setDuration(duration);
+        work.setPrice(price);
+        workRepository.save(work);
+        return "redirect:/admin/serviceDetails";
     }
 
     /*@GetMapping("admin/addService")
--- src/main/java/ru/khaustov/coursediplom/repository/WorkRepository.java
+++ src/main/java/ru/khaustov/coursediplom/repository/WorkRepository.java
@@ -1,7 +1,8 @@
 package ru.khaustov.coursediplom.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import ru.khaustov.coursediplom.entity.Work;
 
-public interface WorkRepository extends CrudRepository<Work, Long> {
+public interface WorkRepository extends CrudRepository<Work, Long>, JpaSpecificationExecutor<Work> {
 }
