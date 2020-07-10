0a533dd9a169c0ddc126194fb89af5648e3422ab
Maksim Khaustov <haustovmaksim13@gmail.com>
2020-07-10 14:43:22 +0300

    new12

--- src/main/java/ru/khaustov/coursediplom/contriollers/WorkController.java
+++ src/main/java/ru/khaustov/coursediplom/contriollers/WorkController.java
@@ -3,7 +3,8 @@ package ru.khaustov.coursediplom.contriollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.khaustov.coursediplom.entity.Work;
 import ru.khaustov.coursediplom.repository.WorkRepository;
 
 @Controller
@@ -17,10 +18,30 @@ public class WorkController {
         return "admin/crudworks";
     }
 
-    @GetMapping("admin/addService")
-    public String addService(Model model){
+
+    @RequestMapping(value = "admin/addService", method = RequestMethod.GET)
+    public String showForm(Model model) {
+        model.addAttribute("work", new Work());
         return "admin/addService";
     }
+
+    @PostMapping("admin/addService")
+    public String submitForm(@ModelAttribute Work work) {
+        workRepository.save(work);
+        return "redirect:/admin/addService";
+    }
+
+    /*@GetMapping("admin/addService")
+    public String addServiceForm(Model model){
+        return "admin/addService";
+    }
+
+    @PostMapping ("admin/addService")
+    public String addService(@RequestParam String typeOfService,
+                             @RequestParam String description, @RequestParam String duration,
+                             @RequestParam Integer price_type, Model model){
+        return "admin/addService";
+    }*/
 
 
 }
